89b19ac7e0a4cdf813c4168b22f721f537e026da
EltiRahmawati19 <59133841+EltiRahmawati19@users.noreply.github.com>
2021-10-22 10:29:11 +0700

    Membuat Post category untuk web

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -2,7 +2,10 @@
 
 @section('container')
 <article>
-  <h2>{{ $post->title }}</h2>
+  <h1>{{ $post->title }}</h1>
+
+  <p>By. Elti Rahmawati in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
  {!! $post->body !!}
     </article>
 
