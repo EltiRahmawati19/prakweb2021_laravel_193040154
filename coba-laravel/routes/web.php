f8922958adf55b770668b9b7a426413354734cb4
EltiRahmawati19 <59133841+EltiRahmawati19@users.noreply.github.com>
2021-10-29 09:52:35 +0700

    Membuat user login untuk halaman login dan register

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
+
 use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -59,3 +60,6 @@ Route::get('/categories/{category:slug}', function(Category $category) {
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
