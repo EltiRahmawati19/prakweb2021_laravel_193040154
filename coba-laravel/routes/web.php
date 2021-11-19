5a4646858fee6b87ef011b100df70493caffe68a
EltiRahmawati19 <59133841+EltiRahmawati19@users.noreply.github.com>
2021-11-19 17:21:24 +0700

    Memperbaharui codingan yang  eror

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 
 /*
@@ -39,30 +41,23 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', [PostController::class, 'index']);
-
-//halaman single post
-
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories', function() {
+Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
-
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'title' => "Post By Category :$category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category','author')
-    ]);
-});
-
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
+
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard.index');
+// })->middleware('auth');
+Route::get('/dashboard', [DashboardController::class, 'index']);
