a330c25503b72dcca3254600bb91490c90ff4445
EltiRahmawati19 <59133841+EltiRahmawati19@users.noreply.github.com>
2021-12-02 19:12:01 +0700

    Menambahkan Edit dan hapus image

--- coba-laravel/app/Http/Controllers/DashboardPostController.php
+++ coba-laravel/app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -105,6 +106,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:10024',
             'body' => 'required'
 
         ];
@@ -113,6 +115,14 @@ class DashboardPostController extends Controller
             $rules['slug'] = 'required|unique:posts';
         }
         $validatedData = $request->validate($rules);
+
+        if($request->file('image')) {
+            if ($request->oldImage) {
+               Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
+
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -131,6 +141,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if ($post->image) {
+            Storage::delete($post->image);
+         }
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post has been added!');
     }
